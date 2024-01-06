1952777bda3c2a915525aae4be15d924a4a1d3c7
vinayasaga <vinaya.reddy@nagarro.com>
2024-01-06 23:56:39 +0530

    added alias configuaration

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
+      components: path.resolve(__dirname, "./src/components/"),
       routes: path.resolve(__dirname, "./src/routes/"),
       services: path.resolve(__dirname, "./src/services/"),
     },
